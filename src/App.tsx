2f5dba9f7ce67645e23b0a25d6f5e8d6d45964af
Roberto Grijalva Sanchez <roberd.999117@gmail.com>
2024-06-03 18:19:53 -0600

    Second safeguard

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
-import { useReducer } from 'react'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import './App.css'
 import { useStore } from './hooks/useStore'
+import { AUTO_LANGUAGE } from './constants'
+import { ArrowsIcon } from './components/Icons'
+import { LanguageSelector } from './components/LanguageSelector'
+import { SectionType } from './assets/types.d'
 
 
 
 function App() {
 
-  const { fromLanguage, setFromLanguage } = useStore()
+  const { fromLanguage, toLanguage, interchangeLanguage, setFromLanguage, setToLanguage } = useStore()
 
     return (
-    <div className='App'>
+    <Container fluid>
       <h1>Google Translate</h1>
-      <button onClick={() => {
-        setFromLanguage('es')
-      }}>Cambiar a Español</button>
-      {fromLanguage}
-    </div>
+
+      <Row>
+        <Col>
+        <LanguageSelector 
+          type={SectionType.From}
+          value={fromLanguage}
+          onChange={setFromLanguage}
+        />
+        {fromLanguage}
+        </Col>
+        
+        <Col>
+          <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguage}>
+            <ArrowsIcon/>
+          </Button>
+        </Col>
+
+        <Col>
+          <LanguageSelector 
+            type={SectionType.To}
+            value={toLanguage}
+            onChange={setToLanguage}
+            />
+          {toLanguage}
+        </Col>
+      </Row>
+
+    </Container>
   )
 }
 
--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,5 +1,6 @@
 import { useReducer } from 'react'
-import { Action, Language, fromLanguage, type State } from '../assets/types'
+import { Action, Language, FromLanguage, type State } from '../assets/types'
+import { AUTO_LANGUAGE } from '../constants'
 
 
 const initialState: State = {
@@ -14,38 +15,39 @@ function reducer(state: State, action: Action){
     const { type } = action
   
     if (type === 'INTERCHANGE_LANGUAGES') {
-      return {
-        ... state,
-        fromLanguage: state.toLanguage,
-        toLanguage: state.fromLanguage,
-      }
+        if (state.fromLanguage === AUTO_LANGUAGE) return state
+        return {
+            ... state,
+            fromLanguage: state.toLanguage,
+            toLanguage: state.fromLanguage,
+        }
     }
     if (type === 'SET_FROM_LANGUAGE') {
-      return {
-        ... state,
-        fromLanguage: action.payload
-      }
+        return {
+            ... state,
+            fromLanguage: action.payload
+        }
     }
     if (type === 'SET_TO_LANGUAGE') {
-      return {
-        ... state,
-        toLanguage: action.payload
-      }
+        return {
+            ... state,
+            toLanguage: action.payload
+        }
     }
     if (type === 'SET_FROM_TEXT') {
-      return {
-        ... state,
-        loading: true,
-        fromText: action.payload,
-        result: ''
-      }
+        return {
+            ... state,
+            loading: true,
+            fromText: action.payload,
+            result: ''
+        }
     }
     if (type === 'SET_RESULT') {
-      return {
-        ... state,
-        loading: false,
-        result: action.payload
-      }
+        return {
+            ... state,
+            loading: false,
+            result: action.payload
+        }
     }
     return state
 }
@@ -58,7 +60,7 @@ export function useStore(){
         dispatch({ type: 'INTERCHANGE_LANGUAGES' })
     }
 
-    const setFromLanguage = (payload: fromLanguage) => {
+    const setFromLanguage = (payload: FromLanguage) => {
         dispatch({ type: 'SET_FROM_LANGUAGE', payload })
     }
 
